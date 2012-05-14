2e409f059ffac4a02043e72583ea172ef5861c2a
tim.tang <tang.jilong@139.com>
2012-05-14 10:03:44 +0800

    modify production env params&add favicon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,10 @@ EverydayCn::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
+  #Use a different logger for distributed setups
+  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 'weekly')
+  config.logger.level = Logger::INFO
+
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
 
@@ -29,7 +33,7 @@ EverydayCn::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
