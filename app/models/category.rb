1e2f7fa5b481ccbff93919a0dfc639bce241f6f0
tim.tang <tang.jilong@139.com>
2012-05-15 09:02:40 +0800

    alter restful api url

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveResource::Base
-  self.site="http://localhost:3000"
+  #self.site="http://localhost:3000"
+  self.site="http://cms.everyday-cn.com"
 
   def self.get_category(categoryId)
     Category.find(:one, :from=>"/category/#{categoryId}.xml")
